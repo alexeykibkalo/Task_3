535c4fd18117fe0ded5924e398473f7a55b08a66
Alexey Kibkalo <alexeykibkalo87@gmail.com>
2017-04-19 01:09:46 +0300

    Task_3_SecondCommit

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -26,52 +26,26 @@ public class MainClass extends TestScripts {
 
         driver.findElement(By.xpath("//li[@data-idx='1']")).click();
 
-        driver.close();
+        Actions builder = new Actions(driver);
+        builder.moveToElement(driver.findElement(By.xpath("//div[@id='miniheader_data']"))).click().build().perform();
+        //Thread.sleep(2000);
+        builder.sendKeys(Keys.ARROW_RIGHT).build().perform();
+       // Thread.sleep(2000);
+        builder.sendKeys(Keys.ARROW_RIGHT).build().perform();
+       // Thread.sleep(2000);
+        builder.sendKeys(Keys.ARROW_RIGHT).build().perform();
+       // Thread.sleep(2000);
+       // builder.click(driver.findElement(By.xpath("//div[@idx='2']"))).build().perform();
+        //builder.moveByOffset(800,600).build().perform();
+       // builder.click().build().perform();
 
-       // WebElement nextImage = (new WebDriverWait(driver, 10))
-               // .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@id= 'iol_navr']")));
-        //nextImage.click();
-
-       //WebElement element =(WebElement)((JavascriptExecutor)driver)
-             //  .executeScript("document.getElementById('b_content').focus();");
-
-       //element.sendKeys("sdsdas");
-
-      //  Actions builder = new Actions(driver);
-      //  builder.moveToElement( driver.findElement(By.xpath("//div[@id='b_content']")));
-       // builder.click();
-      //  builder.sendKeys(Keys.ARROW_RIGHT);
-       // builder.build().perform();
-
-//div[@id='b_content']
-       // Thread.sleep(5000);
-
-      //  WebElement nextImage = (new WebDriverWait(driver, 100))
-     //    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@class='iol_nav' and @id = 'iol_navr']")));
-        //WebElement element =(WebElement)((JavascriptExecutor)driver)
-        //  .executeScript("document.getElementById('miniheader').focus();");
-  // driver.findElement(By.xpath("//div[@id = 'switcher']"));
-
-     // WebElement temp =  driver.findElement(By.xpath("//div[@id = 'b_content']"))
-     //    .findElement(By.id("detail_canvas")).findElement(By.id("iol_navl"));
-
-//        Actions builder = new Actions(driver);
-   //      builder.contextClick(temp).build().perform();
-        // builder.click();
-        //  builder.sendKeys(Keys.ARROW_RIGHT);
-        // builder.build().perform();
-//driver.findElement(By.xpath("//div[@id = 'b_content']"))
-// .findElement(By.id("detail_canvas")).findElement(By.id("iol_navl")).click()
-
-       // driver.findElement(By.xpath("//div[@id = 'b_content'")).click();
-
-     //   WebElement el = driver.findElement(By.xpath("//a[@class='iol_nav' and @id = 'iol_navr']"));
-
-      //  el.click();
+        //driver.findElement(By.xpath("//input[@id='sb_form_q']")).click();
 
 
-       // driver.findElement(By.xpath("//div[@id='can_fav']")).click();
 
+
+
+      // WebElement el = driver.findElement(By.xpath("//a[@class='iol_nav' and @id = 'iol_navr']"));
 
 
     }
